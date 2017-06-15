1e05dae7ef3d3345b29157625eb79f6fb3dbe57d
Darkgrouptw <darkgrouptw@gmail.com>
2017-06-15 10:20:21 +0800

    測試 Unity 5.6 的 Video Player

--- Assets/Scripts/Recorder/GPSLocationManager.cs
+++ Assets/Scripts/Recorder/GPSLocationManager.cs
@@ -9,18 +9,9 @@ public class GPSLocationManager : MonoBehaviour
 {
     [Header("========== GPS 記錄相關 ==========")]
     public string URL = "http://140.118.9.179/userLocation/";
-
-	public GameObject HelpPanel;
-    public RawImage LoadingTexture;
-    public MovieTexture LoadingMovie;
-
+    
     private void Start()
     {
-        // Loading 的圖片
-        LoadingTexture.texture = LoadingMovie;
-        LoadingMovie.loop = true;
-        LoadingMovie.Play();
-
         StartCoroutine(GPSRecord());
     }
 
@@ -87,7 +78,6 @@ public class GPSLocationManager : MonoBehaviour
                 {
                     // 發現有人需要幫忙，要傳送影片，先顯示視窗
                     Debug.Log("HelpID => " + DataList["helpID"]);
-					HelpPanel.SetActive (true);
                 }
 
                 yield return new WaitForSeconds(20);
